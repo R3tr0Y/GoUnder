c1e1f86cb489b1f8966cf5e09ef913b0ab9508f7
r3tr0 <siriusms.black@yahoo.com>
2025-08-08 16:26:20 +0800

    add cdn rule: Byte-nginx (ByteDance cdn)

--- utils/rule.go
+++ utils/rule.go
@@ -1,13 +1,12 @@
 package utils
 
 func FofaRules() string {
-	return `&& server!="cloudflare" && server!="alicdn" &&` +
-		` server!="qcloud" && server!="yunjiasu" && server!="yupaicloud"` +
-		` && server!="upyun" && server!="ws" && server!="cdnws" &&` +
-		` server!="china cache" && server!="fastly" && server!="akamai"` +
+	return `&& server!="cloudflare" && server!="alicdn" && server!="qcloud"` +
+		` && server!="yunjiasu" && server!="yupaicloud" && cloud_name!="Cloudflare"` +
+		` && server!="upyun" && server!="ws" && server!="cdnws"` +
+		` && server!="china cache" && server!="fastly" && server!="akamai"` +
 		` && server!="akamaighost" && server!="cloudfront" && server!="hwcdn"` +
 		` && server!="wangzhansheshi" && server!="360wzws" && server!="incapsula"` +
 		` && server!="stackpath" && server!="keycdn" && cloud_name!="cloudfront"` +
-		` && org!="CLOUDFLARENET" && server!="layun.com" && server!="*cdn*"` +
-		` && cloud_name!="Cloudflare"`
+		` && org!="CLOUDFLARENET" && server!="layun.com" && server!="*cdn*" && server!="Byte-nginx"`
 }
